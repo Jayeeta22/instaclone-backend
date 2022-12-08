7016edd49fb66f57df7c64dfed1ab98f7e0da631
Jayeeta22 <117907901+Jayeeta22@users.noreply.github.com>
2022-12-08 09:41:07 +0530

    modified

--- src/Connection/connect.js
+++ src/Connection/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(
     "mongodb+srv://jayeeta22:instaclone@cluster0.fdjljsc.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
-    console.log("Login successful");
+    console.log("connection successful");
   })
   .catch((err) => {
     console.log(err);
